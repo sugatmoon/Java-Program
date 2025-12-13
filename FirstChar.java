259a5ae9a4e96606c11618a1907ee75a74f60d77
sugatmoon <sugatmoon17@gmail.com>
2025-12-13 09:56:03 +0530

    tast completed

---
+++ FirstChar.java
@@ -0,0 +1,4 @@
+void main(String [] args){
+    IO.println(args[0].charAt(0));
+
+}
---
+++ StringToInt.java
@@ -0,0 +1,6 @@
+void main(String[] args) {
+    int num1 = Integer.parseInt(args[0]);
+    int num2 = Integer.parseInt(args[1]);
+    IO.print(num1 + num2);
+
+}
