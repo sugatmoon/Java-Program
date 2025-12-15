8e972a4d84aa177523c535697e02e18ac299972d
sugatmoon <sugatmoon17@gmail.com>
2025-12-15 10:30:55 +0530

    all dought clear

---
+++ Binary.java
@@ -0,0 +1,5 @@
+void main() {
+    int num = 0B1010;
+    IO.println();
+    IO.println(num);
+}
---
+++ Unicode.java
@@ -0,0 +1,13 @@
+void main() {
+    char a = 'A';
+    char b = '\u0041'; // 16^0*1+16^1*4=1+64=65
+    char c = 65;
+    char d = '\n';
+    char e = ' ';
+    IO.println();
+    IO.println(a);
+    IO.println(b);
+    IO.println(c);
+    IO.println(d);
+    IO.println(e);
+}
