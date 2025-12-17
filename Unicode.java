a5c850fabc117e41fd28178b3a7d9b0a3ef34134
sugatmoon <sugatmoon17@gmail.com>
2025-12-17 10:11:23 +0530

    completed task

--- Unicode.java
+++ Unicode.java
@@ -1,13 +1,161 @@
 void main() {
-    char a = 'A';
-    char b = '\u0041'; // 16^0*1+16^1*4=1+64=65
-    char c = 65;
-    char d = '\n';
-    char e = ' ';
-    IO.println();
-    IO.println(a);
-    IO.println(b);
-    IO.println(c);
-    IO.println(d);
-    IO.println(e);
+    // char a = 'A';
+    // char b = '\u0041'; // 16^0*1+16^1*4=1+64=65
+    // char c = 65;
+    // char d = '\n';
+    // char e = ' ';
+    // IO.println();
+    // IO.println(a);
+    // IO.println(b);
+    // IO.println(c);
+    // IO.println(d);
+    // IO.println(e);
+
+
+        // int a=9;
+        // float b = a/2;
+        // IO.println(b);
+
+        // char ch = 'A';
+        // int a = ch + 1;
+        // ch = (char)a;
+        // IO.println(ch);
+
+        // float a = 8.2/2;
+        // IO.println(a);
+        // int a 
+       
+
+
+        // short a = (short)65540;
+        // IO.println(a);
+
+
+        
+        // int a = 300;
+        // long b = a;
+        // IO.println(b);
+
+        // char ch = 'c';
+        // int i = ch;
+        // IO.println(i
+        // );
+
+    
+        // IO.print("Y"+"O");
+        // IO.println('L');
+        // IO.println('O');
+
+        // IO.println("Y"+"O");
+        // IO.println('L' + 'O');
+
+
+        // int i = 100;
+        // long l = i;
+        // float f = l;
+        // IO.println("Int value " + i);
+        // IO.println("Long value " + l);
+        // IO.println("Float value " + f);
+
+
+
+
+        // char ch = 'c';
+        // int num = 88;
+        // ch =(char) num;
+        // IO.print(ch);
+
+        // byte b = 42;
+        // char c = 'a';
+        // short s = 1024;
+        // int i = 50000;
+        // IO.println(i);
+        // float f = 5.67f;
+        // double d = .1234;
+        // double result = (f * b) + (i / c) - (d * s);
+        // IO.println("result = " + result);
+
+
+        // int x = 10;
+        // if (x == 20) 
+        // {  
+        //     IO.println("Hello");
+        // }
+
+
+        // int a = 5;
+        // if (a++ == 5)
+        //     if (a++ == 6)
+        //         IO.println("Yes");
+        //     else
+        //         IO.println("No");
+        //     else
+        //         IO.println("Maybe");
+
+
+
+//         boolean flag = false;
+// if (flag = true)
+//     IO.println("True");
+// else
+//     IO.println("False");
+
+
+
+
+    // String s = "Java";
+    // if (s == "Java")
+    //     IO.println("Match 1");
+    // if (s.equals("Java"))
+    //      IO.println("Match 2");
+
+
+
+            //     int x = 10;
+
+            // if (x > 5)
+            //     if (x < 9)
+            //         IO.println("A");
+            //     else
+            //         IO.println("B");
+            // else
+            //     IO.println("C");
+
+
+        // int n = 0;
+
+        // if (n == 1 || n == 0)
+        //     IO.println("Yes");
+        // else
+        //     IO.println("No");
+
+
+
+        //     char c = 'A';
+
+        // if (c >= 'a' && c <= 'z')
+        //     IO.println("Lower");
+        // else if (c >= 'A' && c <= 'Z')
+        //     IO.println("Upper");
+
+
+
+        // boolean a = true;
+        // boolean b = false;
+
+        // if (a & b)
+        //     IO.println("1");
+        // if (a && b)
+        //     IO.println("2");
+        // if (a | b)
+        // IO.println("3");
+        // if (a || b)
+        //     IO.println("4");
+
+
+
+
+
+
+
 }
