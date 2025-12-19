4c51a0e98206aa0990986c8659fc9a37c2756045
sugatmoon <sugatmoon17@gmail.com>
2025-12-19 10:15:15 +0530

    assignment completed

---
+++ LabAssign19.java
@@ -0,0 +1,148 @@
+void main() {
+    // char ch ='A';
+    // ch--;
+    // ch--;
+    // ch--;
+    // IO.println(ch); 
+
+
+    // double d = 15.15;
+    // d++;
+    // IO.println(d);
+
+
+
+
+    // int x = 1;
+    // IO.println(x);
+    // IO.println(x++);
+    // IO.println(x);
+    // IO.println(++x);
+    // IO.println(x);
+
+
+    // int a = 5;
+    // IO.println(a--);
+    // IO.println(a);
+    // IO.println(--a);
+    // IO.println(a++);
+    // IO.println(a);
+
+
+
+        // int p = 10;
+        // IO.println(p++);//11
+        // IO.println(++p);//12
+        // IO.println(p--);//
+        // IO.println(--p);
+        // IO.println(p);
+
+
+        // int i = 9;
+        // int j = i++ + 5;
+        // IO.println(i+" : "+j);
+
+
+
+        // int i = 9;
+        // int j = ++i + 5;
+        // IO.println(i+" : "+j);
+
+
+
+        // int i = 5;
+        // int j = --i - 4;
+        // IO.println(i+" : "+j);
+
+
+
+        // int i = 3;
+        // int result = i++ + ++i;   
+        //      //      3 -> 4 + 5
+        // IO.println("i = " + i);      
+        // IO.println("result = " + result); 
+
+
+//         int a = 9;
+//         int b = a++ + a++;  
+// //              9->10 + 10 -> 11
+//         IO.println(a); 
+//         IO.println(b); 
+
+
+
+
+//         int x = 10;
+//         int y = ++x + x++;
+// //              11 + 11 -> 12
+//         IO.println(x);
+//         IO.println(y);
+
+
+
+
+
+
+//         int x = 3;
+//         int y = ++x + x++ + --x;
+// //             4 + 4-> 5 + 4
+//         IO.println(x);
+//         IO.println(y);
+
+
+
+//         int a = 4;
+// //                 4-> 5 + 6
+//         IO.println(a++ + ++a);
+//         IO.println(a);
+
+
+
+//             int k = 3;
+//             int r = k++ * ++k / k--;  
+// //                  3->4 * 5 / 5 -> 4
+//             IO.println(k); 
+//             IO.println(r); 
+
+
+
+            // int x = 3;
+            // int y = ++x * 5 / x--; 
+            // //      4 * 5 / 4->3  
+            // IO.println(x+" : "+y);
+
+
+
+
+
+            // int a = 4;
+            // int b = a++ * 2 / --a;
+            
+            
+            // //      4 -> 5 * 2 / 4
+            // IO.println(a+" : "+b); 
+
+
+
+            // int n = 5;
+            // int result = ++n * 2 / n-- * n; 
+            // //           6 * 2 / 6->5 * 5
+
+            // IO.println(n); 
+            // IO.println(result); 
+
+
+
+
+//                 int a = 5;
+//                 int b = a++ * 4 / ++a;
+// //                      5->6 * 4 / 7
+//                 IO.println(a);
+//                 IO.println(b);
+
+
+
+
+
+
+}
