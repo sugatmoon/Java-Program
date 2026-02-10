6aa1fef86d9dd3ac01993dcbc7011d669b03a0d0
sugatmoon <sugatmoon17@gmail.com>
2026-02-10 20:53:43 +0530

    Practice

---
+++ Geometric/VariousObj.java
@@ -0,0 +1,24 @@
+package Geometric;
+class A {
+
+    int a = 3;
+    int b = 7;
+    
+}
+class B {
+int c = 2;
+int d = 9;
+    
+}
+public class VariousObj {
+    
+
+    public static void main(String[] args) {
+        A a = new A();
+
+
+    
+
+    }
+
+}
